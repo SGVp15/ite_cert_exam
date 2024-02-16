ed27003e3a594748013406a6051cecfd4c45c3c0
SGVp15 <phosphorusp15@mail.ru>
2024-02-16 13:29:30 +0300

    start

--- utils/Progress_bar.py
+++ utils/Progress_bar.py
@@ -2,5 +2,5 @@ def progress(text='', percent=0, width=30):
     left = width * percent // 100
     right = width - left
     print(f"\r{text}[{'#' * left}{' ' * right}]",
-          f' {percent:.0f}%',
+          f' {percent:.0f}\t%',
           sep='', end='', flush=True)
