6c2fb5dcf395925d777b2f35de65439d4603d6eb
SGVp15 <phosphorusp15@mail.ru>
2024-09-09 17:19:41 +0300

    email

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
-_base_path = os.path.join('//192.168.20.100', 'Administrative server', 'РАБОТА АДМИНИСТРАТОРА',
+BASE_PATH = os.path.join('//192.168.20.100', 'Administrative server', 'РАБОТА АДМИНИСТРАТОРА',
                           'ОРГАНИЗАЦИЯ IT ЭКЗАМЕНОВ', 'ЭКЗАМЕНЫ ЦИФРОВОЙ ПУТЬ')
-FILE_XLSX = os.path.join(_base_path, 'Нумерация_Экзамены.xlsx')
-OUT_DIR = os.path.join(_base_path, 'сертификаты')
-TEMPLATE_FOLDER = os.path.join(_base_path, 'templates')
+FILE_XLSX = os.path.join(BASE_PATH, 'Нумерация_Экзамены.xlsx')
+OUT_DIR = os.path.join(BASE_PATH, 'сертификаты')
+TEMPLATE_FOLDER = os.path.join(BASE_PATH, 'templates')
 
-EMAIL_WEB_MANAGER = ['an.kuznetsov@itexpert.ru',]
-EMAIL_BCC = ['g.savushkin@itexpert.ru', 'o.kuprienko@itexpert.ru']
+EMAIL_WEB_MANAGER = ['an.kuznetsov@itexpert.ru', ]
+EMAIL_BCC = ['g.savushkin@itexpert.ru', 'o.kuprienko@itexpert.ru', 'p.moiseenko@itexpert.ru']
 
 os.makedirs(OUT_DIR, exist_ok=True)
 
@@ -15,4 +15,3 @@ LOG_FILE = './log.txt'
 
 PICKLE_USERS = './users.pk'
 PICKLE_FILE_MODIFY = './time_file_modify.pk'
-
