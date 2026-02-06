4f2b23c11f60b20d90a8b1c0100994d7d791ad56
SGVp15 <phosphorusp15@gmail.com>
2026-02-06 23:52:24 +0300

    main

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ BASE_PATH = Path(
     '//192.168.20.100/Administrative server/РАБОТА АДМИНИСТРАТОРА/ОРГАНИЗАЦИЯ IT ЭКЗАМЕНОВ/ЭКЗАМЕНЫ ЦИФРОВОЙ ПУТЬ')
 
 FILE_XLSX = BASE_PATH / 'Нумерация_Экзамены.xlsx'
-OUT_DIR = BASE_PATH / 'сертификаты'
+DIR_CERTS = BASE_PATH / 'сертификаты'
 TEMPLATE_FOLDER = BASE_PATH / 'template_cert_png'
 
-OUT_DIR.mkdir(parents=True, exist_ok=True)
+DIR_CERTS.mkdir(parents=True, exist_ok=True)
 TEMPLATE_FOLDER.mkdir(parents=True, exist_ok=True)
 
 LOG_FILE = Path('./log.txt')
